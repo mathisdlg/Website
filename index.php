f68426d5e90ec26b466b4245b60b1ba22841815a
mathisdlg <delage.mathis.1@gmail.com>
2024-05-23 13:34:00 +0200

    Add base of the website

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Redirect</title>
+    <meta http-equiv="refresh" content="0; URL=src/home.php">
+</head>
+
+<body>
+    <p>If your not redirect click <a href="src/home.php">here</a></p>
+</body>
+
+</html>
---
+++ src/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <h1>
+        Welcome to my personal website!
+    </h1>
+
+    <?php
+        include 'fragments/menu.html';
+    ?>
+
+</body>
+
+</html>
